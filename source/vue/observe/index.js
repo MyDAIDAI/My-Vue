eddd5c81358e9f74932d5367ee4736c5c7b5cd2c
dengpan <571143755@qq.com>
2020-04-04 15:41:31 +0800

    [add]添加数据劫持

--- source/vue/observe/index.js
+++ source/vue/observe/index.js
@@ -16,9 +16,22 @@ export function observe(data) {
   if (typeof data !== 'object' || data == null) return;
   new Observe(data)
 }
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key]
+    },
+    set(newVal) {
+      vm[source][key] = newVal
+    }
+  })
+}
 function initData(vm) {
   let data = vm.$options.data
   data = vm._data = typeof data === 'function' ? data.call(vm) : data || {}
+  for(let key in vm._data) {
+    proxy(vm, '_data', key)
+  }
   observe(vm._data)
 }
 function initComputed(vm) {}
